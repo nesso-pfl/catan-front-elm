0f54e61ba991078fac03a4bce64ec45ded3e036e
nesso-pfl <ghbjy6@gmail.com>
2020-02-13 17:00:26 +0900

    Add Catan.Core

--- src/Catan.elm
+++ src/Catan.elm
@@ -1,47 +1,82 @@
 module Catan exposing (main)
 
 import Browser
+import Catan.Core exposing (..)
 import Html exposing (Html, text)
 
 
+
 -- MAIN
+
+
 main =
     Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
 -- MODEL
-type alias Model
-    = {}
 
 
-init : () -> (Model, Cmd Msg)
+type alias Model =
+    { panels : List Panel
+    , harbors : List Harbor
+    , robber : Point
+    , roads : List Road
+    , buildings : List Building
+    , diceResult : Int
+    , phase : Phase
+    , turn : String
+    , user : User
+    , others : List User
+    }
+
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-    ( {}
+    ( { panels = []
+      , harbors = []
+      , robber = { x = 0, y = 0 }
+      , roads = []
+      , buildings = []
+      , diceResult = 0
+      , phase = Dice
+      , turn = ""
+      , user =
+            Self
+                { id = ""
+                , resources = []
+                , cards = []
+                }
+      , others = []
+      }
     , Cmd.none
     )
 
 
 
 -- UPDATE
+
+
 type Msg
     = Dummy
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg _ =
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
     case msg of
         Dummy ->
-            ({}, Cmd.none)
+            ( model, Cmd.none )
 
 
 
 -- SUBSCRIPTIONS
+
+
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
@@ -49,6 +84,8 @@ subscriptions _ =
 
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view _ =
     text "Catan"
---
+++ src/Catan/Core.elm
@@ -0,0 +1,74 @@
+module Catan.Core exposing (..)
+
+
+type alias Point =
+    { x : Int
+    , y : Int
+    }
+
+
+type Resource
+    = Desert
+    | Brick
+    | Lumber
+    | Wool
+    | Grain
+    | Ore
+
+
+type alias Panel =
+    { point : Point
+    , resource : Resource
+    , num : Int
+    }
+
+
+type alias Harbor =
+    { point : Point
+    , resource : Resource
+    }
+
+
+type Road
+    = Road Point
+
+
+type Building
+    = Settlement Point
+    | City Point
+
+
+type User
+    = Self
+        { id : String
+        , resources : List Resource
+        , cards : List Card
+        }
+    | Other
+        { id : String
+        , resourceNum : Int
+        , cardNum : Int
+        }
+
+
+type Card
+    = VictoryPoint
+    | Knight
+    | Monopoly
+
+
+type Phase
+    = Dice
+    | Yield
+    | Thief
+    | Resource
+    | Build
+    | Card
+    | Trade
+    | Victory
+    | End
+
+
+dice : Int -> Int -> Int
+dice n m =
+    n + m
